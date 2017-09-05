036922070d6e14fdc40a72eb09329a2419bc7fe0
Zule <zulevp94@hotmail.com>
2017-09-05 11:30:42 -0400

    Acerca de

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
+import { PerfilPage } from '../pages/perfil/perfil';
+import { PreventaPage } from '../pages/preventa/preventa';
+import { AutoVentaPage } from '../pages/auto-venta/auto-venta';
 import { InfoMapPage } from '../pages/info-map/info-map';
+import { ListPage } from '../pages/list/list';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,9 +26,12 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Preventa', component: HomePage },
+      { title: 'Perfil', component: PerfilPage },
+      { title: 'Preventista', component: PreventaPage },
+      { title: 'Auto Venta', component: AutoVentaPage },
+      { title: 'Mapa', component: InfoMapPage },
       { title: 'Historial', component: ListPage },
-      {title: 'Mapa', component: InfoMapPage }
+      { title: 'Salir', component: HomePage }
     ];
 
   }
